ac9bdfecfcf120bf9bac0bfb3468bfff7b6a846f
MARIO BENJAMIN MONTERO CEDILLO <mario.montero@epn.edu.ec>
2021-01-06 01:09:42 -0500

    #12 relaciones completas

--- app/User.php
+++ app/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable implements JWTSubject
     ];
     public function getJWTIdentifier()     {       return $this->getKey();     }
     public function getJWTCustomClaims()     {       return [];     }
+    public function articles() {     return $this->hasMany('App\Article'); }
+    public function comments() {     return $this->hasMany('App\Comment');
+    }
+    public function categories()     {
+        return $this->belongsToMany('App\Category')->withTimestamps();
+    }
 }
